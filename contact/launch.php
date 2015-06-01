5e2ae8c8aea3c59fc6b5647e8f43f006889ca18d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-06-01 09:25:02 +0700

    Include Backend Stuff Only in Backend

--- contact/launch.php
+++ contact/launch.php
@@ -3,7 +3,7 @@
 // Load the configuration data
 $contact_config = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt')->unserialize();
 
-if($config->url_path === $contact_config['slug']) {
+if(Route::is($contact_config['slug'])) {
 
     Weapon::add('shell_after', function() {
         echo O_BEGIN . '<style>
@@ -151,35 +151,3 @@ if($config->url_path === $contact_config['slug']) {
     });
 
 }
-
-
-/**
- * Plugin Updater
- * --------------
- */
-
-Route::accept($config->manager->slug . '/plugin/' . basename(__DIR__) . '/update', function() use($config, $speak) {
-    if( ! Guardian::happy()) {
-        Shield::abort();
-    }
-    if($request = Request::post()) {
-        Guardian::checkToken($request['token']);
-        // Check for invalid email address
-        if( ! empty($request['email_recipient']) && ! Guardian::check($request['email_recipient'])->this_is_email) {
-            Notify::error($speak->notify_invalid_email);
-        }
-        $request['email_recipient'] = Text::parse($request['email_recipient'])->to_ascii;
-        if( ! isset($request['html_parser'])) $request['html_parser'] = false;
-        unset($request['token']); // Remove token from request array
-        if( ! Notify::errors()) {
-            File::serialize($request)->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt');
-            Notify::success(Config::speak('notify_success_updated', array($speak->plugin)));
-            Session::kill('error_input');
-        } else {
-            // `Notify::errors()` cannot be used after redirection, so I use
-            //  sessions for handling the `Guardian::wayback()` in `configurator.php`
-            Session::set('error_input', true);
-        }
-        Guardian::kick(dirname($config->url_current));
-    }
-});
